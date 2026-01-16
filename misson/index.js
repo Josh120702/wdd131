6c25ea8fcae964a2f37ce380dc88d3637c6371e0
Josh120702 <jcyoung1207@gmail.com>
2026-01-16 15:08:43 -0700

    9

--- misson/index.js
+++ misson/index.js
@@ -6,10 +6,15 @@ selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current == 'dark') {
+    if (current === 'dark') {
         // code for changes to colors and logo
+        document.body.style.backgroundColor =" #000";
+        logo.src = "https://wddbyui.github.io/wdd131/images/byui-logo-white.png";
+        document.body.style.color = "#fdfdfdff"
     } else {
         // code for changes to colors and logo
+        document.body.style.color = "#000";
+        document.body.style.backgroundColor = "#fdfdfdff";
     }
 }           
                     
